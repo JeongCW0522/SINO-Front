bfd0b62017ebc5818c464afc1632199345399c3a
정찬원 <jcw0522@gachon.ac.kr>
2025-12-27 15:01:51 +0900

    feat: 온보딩 페이지 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import Hompage from './pages/Hompage';
+import OnBoadingPage from './pages/OnBoardingPage';
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Hompage />,
+      },
+      {
+        path: 'onboarding',
+        element: <OnBoadingPage />,
       },
     ],
   },
