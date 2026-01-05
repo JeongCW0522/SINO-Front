2e111f20e249e92b9c25d6ebfb5be2965dcc2e49
정찬원 <jcw0522@gachon.ac.kr>
2026-01-06 02:06:54 +0900

    feat: 에러 페이지 라우트 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import Hompage from './pages/Hompage';
 import OnBoadingPage from './pages/OnBoardingPage';
+import ErrorPage from './pages/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
