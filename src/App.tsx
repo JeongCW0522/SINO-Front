b0cdbf740a4a43fac35dbe60c8e5cd2c00fe152f
CHANWON <jcw0522@gachon.ac.kr>
2026-01-21 17:04:22 +0900

    feat: 온보딩 페이지에서 헤더바 표시 안되도록 조건 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import NavBar from './components/layouts/NavBar';
 
 function App() {
+  const location = useLocation();
+  const isOnboarding = location.pathname === '/onboarding';
+  const isSplash = location.pathname === '/splash';
+
   return (
     <div className='bg-bgColor min-h-screen'>
-      <NavBar />
+      {!isOnboarding && !isSplash && <NavBar />}
       <Outlet />
     </div>
   );
